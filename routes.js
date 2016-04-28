dd63d16819b10a598d2ac50b46db26b30a2f9e99
Francesco Bedussi <francesco@francescobedussi.it>
2016-04-28 13:07:04 +0200

    Delete file

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var getEnvironments = require('./api/getEnvironments.js');
-var getTags = require('./api/getTags.js');
 var getDir = require('./api/getDir.js');
 var startTest = require('./api/startTest');
 var config = require('./config.js');
@@ -14,12 +13,6 @@ module.exports = function(app) {
         getEnvironments(config.startingFolder, function(err, data) {
             res.send(data);
         })
-    });
-
-    app.get('/tags', function(req, res) {
-        getTags(function(err, data) {
-            res.send(data);
-        });
     });
     
     app.get('/features', function(req, res) {
